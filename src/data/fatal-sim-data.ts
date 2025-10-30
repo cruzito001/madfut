6a44e057fc42f2e47942fe317032b74997a520aa
alancruz0306 <alancruz03063734@tecmilenio.mx>
2025-10-29 18:23:29 -0600

    Update fatal-sim series

--- src/data/fatal-sim-data.ts
+++ src/data/fatal-sim-data.ts
@@ -13,65 +13,43 @@ const fatalSimSeries: FatalSimSeries[] = [
   {
     name: "Series 78",
     seriesNumber: 78,
-    solutions: [
-      { imageUrl: "/fatal-sim/series78/1.jpg" },
-      { imageUrl: "/fatal-sim/series78/2.jpg" },
-    ],
+    solutions: [{ imageUrl: "/fatal-sim/series78/1.png" }],
     order: 1,
   },
   {
     name: "Series 81",
     seriesNumber: 81,
-    solutions: [
-      { imageUrl: "/fatal-sim/series81/1.jpg" },
-      { imageUrl: "/fatal-sim/series81/2.jpg" },
-    ],
+    solutions: [{ imageUrl: "/fatal-sim/series81/1.png" }],
     order: 2,
+  },
+  {
+    name: "Series 83",
+    seriesNumber: 83,
+    solutions: [{ imageUrl: "/fatal-sim/series83/1.png" }],
+    order: 3,
   },
   {
     name: "Series 85",
     seriesNumber: 85,
-    solutions: [
-      { imageUrl: "/fatal-sim/series85/1.jpg" },
-      { imageUrl: "/fatal-sim/series85/2.jpg" },
-    ],
-    order: 3,
-  },
-  {
-    name: "Series 88",
-    seriesNumber: 88,
-    solutions: [
-      { imageUrl: "/fatal-sim/series88/1.jpg" },
-      { imageUrl: "/fatal-sim/series88/2.jpg" },
-    ],
+    solutions: [{ imageUrl: "/fatal-sim/series85/1.png" }],
     order: 4,
   },
   {
-    name: "Series 90",
-    seriesNumber: 90,
-    solutions: [
-      { imageUrl: "/fatal-sim/series90/1.jpg" },
-      { imageUrl: "/fatal-sim/series90/2.jpg" },
-    ],
+    name: "Series 89",
+    seriesNumber: 89,
+    solutions: [{ imageUrl: "/fatal-sim/series89/1.png" }],
     order: 5,
   },
   {
-    name: "Series 93",
-    seriesNumber: 93,
-    solutions: [
-      { imageUrl: "/fatal-sim/series93/1.jpg" },
-      { imageUrl: "/fatal-sim/series93/2.jpg" },
-    ],
+    name: "Series 91",
+    seriesNumber: 91,
+    solutions: [{ imageUrl: "/fatal-sim/series91/1.png" }],
     order: 6,
   },
   {
     name: "Series X",
     seriesNumber: 99, // Using 99 for Series X to maintain sorting
-    solutions: [
-      { imageUrl: "/fatal-sim/seriesX/1.jpg" },
-      { imageUrl: "/fatal-sim/seriesX/2.jpg" },
-      { imageUrl: "/fatal-sim/seriesX/3.jpg" },
-    ],
+    solutions: [{ imageUrl: "/fatal-sim/seriesX/1.png" }],
     order: 7,
   },
 ];

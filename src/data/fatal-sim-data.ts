5c059a2b61341f429978d87d89c8cce7ac710b9a
alancruz0306 <alancruz03063734@tecmilenio.mx>
2025-11-19 16:25:04 -0600

    Update fatal-sim and fatal my club series

--- src/data/fatal-sim-data.ts
+++ src/data/fatal-sim-data.ts
@@ -11,49 +11,47 @@ interface FatalSimSeries {
 
 const fatalSimSeries: FatalSimSeries[] = [
   {
-    name: "Series 67",
-    seriesNumber: 67,
-    solutions: [{ imageUrl: "/fatal-sim/series67/1.jpg" }],
+    name: "Series 75",
+    seriesNumber: 75,
+    solutions: [
+      { imageUrl: "/fatal-sim/75-1.jpg" },
+      { imageUrl: "/fatal-sim/75-2.jpg" },
+    ],
     order: 1,
   },
   {
-    name: "Series 76",
-    seriesNumber: 76,
-    solutions: [{ imageUrl: "/fatal-sim/series76/1.jpg" }],
+    name: "Series 77",
+    seriesNumber: 77,
+    solutions: [{ imageUrl: "/fatal-sim/77-1.jpg" }],
     order: 2,
   },
   {
-    name: "Series 83",
-    seriesNumber: 83,
-    solutions: [
-      { imageUrl: "/fatal-sim/series83/1.jpg" },
-      { imageUrl: "/fatal-sim/series83/2.jpg" },
-    ],
+    name: "Series 81",
+    seriesNumber: 81,
+    solutions: [{ imageUrl: "/fatal-sim/81-1.jpg" }],
     order: 3,
   },
   {
-    name: "Series 86",
-    seriesNumber: 86,
-    solutions: [{ imageUrl: "/fatal-sim/series86/1.jpg" }],
+    name: "Series 85",
+    seriesNumber: 85,
+    solutions: [{ imageUrl: "/fatal-sim/85-1.jpg" }],
     order: 4,
   },
   {
-    name: "Series 89",
-    seriesNumber: 89,
+    name: "Series 88",
+    seriesNumber: 88,
     solutions: [
-      { imageUrl: "/fatal-sim/series89/1.jpg" },
-      { imageUrl: "/fatal-sim/series89/2.jpg" },
-      { imageUrl: "/fatal-sim/series89/3.jpg" },
+      { imageUrl: "/fatal-sim/88-1.jpg" },
+      { imageUrl: "/fatal-sim/88-2.jpg" },
     ],
     order: 5,
   },
   {
-    name: "Series 91",
-    seriesNumber: 91,
+    name: "Series 92",
+    seriesNumber: 92,
     solutions: [
-      { imageUrl: "/fatal-sim/series91/1.jpg" },
-      { imageUrl: "/fatal-sim/series91/2.jpg" },
-      { imageUrl: "/fatal-sim/series91/3.jpg" },
+      { imageUrl: "/fatal-sim/92-1.jpg" },
+      { imageUrl: "/fatal-sim/92-2.jpg" },
     ],
     order: 6,
   },
@@ -61,8 +59,8 @@ const fatalSimSeries: FatalSimSeries[] = [
     name: "Series X",
     seriesNumber: 99, // Using 99 for Series X to maintain sorting
     solutions: [
-      { imageUrl: "/fatal-sim/seriesX/1.jpg" },
-      { imageUrl: "/fatal-sim/seriesX/2.jpg" },
+      { imageUrl: "/fatal-sim/X-1.jpg" },
+      { imageUrl: "/fatal-sim/X-2.jpg" },
     ],
     order: 7,
   },
